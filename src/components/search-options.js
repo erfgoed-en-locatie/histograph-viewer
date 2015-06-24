4a5c1a1c991c7cde032a6a3be9a9ec630fe5f9a3
Ivo de Kler <ivo.de.kler@gmail.com>
2015-06-24 12:10:39 +0200

    return false to hide search options

--- src/components/search-options.js
+++ src/components/search-options.js
@@ -3,16 +3,54 @@
 var React = require('react');
 
 module.exports = React.createClass({
+  getInitialState: function(){
+    return {
+      enabled: false,
+      fromDate: 0,
+      untilDate: +new Date().getFullYear()
+    };
+  },
   render: function() {
-    return null;
+    return false;
     return (
       <div id='options-container' className='box-container'>
         <div className="box-container-padding">
-          <div id='search-options' className='box padding'>
-           Hier komen opties
-          </div>
+          <button className="box" onClick={this.toggle}>{ this.state.enabled ? 'Disable' : 'Enable' } advanced searching</button>
+          { ( this.state.enabled ?
+            <div id='search-options' className='box padding'>
+             <label>from year: {this.state.fromDate}</label><input type="range" min="0" max={new Date().getFullYear()} value={this.state.fromDate} onChange={this.getStateChanger.call(this, 'fromDate', 'untilDate')} />
+             <label>until year: {this.state.untilDate}</label><input type="range" min="0" max={new Date().getFullYear()} value={this.state.untilDate} onChange={this.getStateChanger.call(this, 'untilDate', 'fromDate')} />
+            </div>
+          :
+            false )}
         </div>
       </div>
     );
-  }
+  },
+  toggle: function(){
+    this.setState({ enabled: !this.state.enabled });
+  },
+  getStateChanger: function(key, crossCheck){
+    return function(e){
+      var value = +e.target.value,
+          propHolder = {};
+      propHolder[key] = value;
+
+      if(crossCheck && crossCheck in {untilDate: true, fromDate: true}){
+        if(crossCheck === 'fromDate'){
+          if(+this.state.fromDate > value){
+            propHolder.fromDate = value;
+          }
+        }
+
+        if(crossCheck === 'untilDate'){
+          if(+this.state.untilDate < value){
+            propHolder.untilDate = value;
+          }
+        }
+      }
+
+      this.setState(propHolder);
+    }.bind(this);
+  },
 });
